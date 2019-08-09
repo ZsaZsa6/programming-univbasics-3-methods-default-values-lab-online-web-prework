988d1d448c293b69b8aa909f9ef665496a183933
Ronda D Cowen <cowenhome5@gmail.com>
2019-08-09 21:36:52 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -8,7 +8,7 @@
 
 def meal_choice(veg1, veg2, protein ="meat")
   puts "What a nutritious meal!"
-  a= "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
   puts a
   return a
 end
